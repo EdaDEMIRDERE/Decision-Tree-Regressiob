dd34cb1fae11405d04b12b852f6f7a52ba27cda9
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-08-28 22:24:17 +0300

    random dataset oluşturulması

--- multioutputDecisionTreeRegression.py
+++ multioutputDecisionTreeRegression.py
@@ -1,3 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.tree import DecisionTreeRegressor
+
+# Create a random dataset
+rng = np.random.RandomState(1)
+X = np.sort(200 * rng.rand(100, 1) - 100, axis=0)
+y = np.array([np.pi * np.sin(X).ravel(), np.pi * np.cos(X).ravel()]).T
+y[::5, :] += 0.5 - rng.rand(20, 2)
