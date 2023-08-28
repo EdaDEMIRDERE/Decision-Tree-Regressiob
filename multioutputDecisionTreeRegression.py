965564ac50303b9f54101b3a3d6e30e96c0c904f
Eda DEMIRDERE <demirdereda@hotmail.com>
2023-08-28 22:24:44 +0300

    farklı derinliklerde modellerin oluşturulup eğitilmesi

--- multioutputDecisionTreeRegression.py
+++ multioutputDecisionTreeRegression.py
@@ -7,3 +7,11 @@ rng = np.random.RandomState(1)
 X = np.sort(200 * rng.rand(100, 1) - 100, axis=0)
 y = np.array([np.pi * np.sin(X).ravel(), np.pi * np.cos(X).ravel()]).T
 y[::5, :] += 0.5 - rng.rand(20, 2)
+
+regr_d2 = DecisionTreeRegressor(max_depth=2)
+regr_d5 = DecisionTreeRegressor(max_depth=5)
+regr_d8 = DecisionTreeRegressor(max_depth=8)
+
+regr_d2.fit(X, y)
+regr_d5.fit(X, y)
+regr_d8.fit(X, y)
